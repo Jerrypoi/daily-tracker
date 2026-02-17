b51b927a6663add44d4f759aacedddc987f512c5
Jerry <jerry.poi.sz@gmail.com>
2026-02-17 15:34:34 +0800

    test

--- backend/crates/server_impl/handler.rs
+++ backend/crates/server_impl/handler.rs
@@ -5,13 +5,18 @@ use diesel::prelude::*;
 use models::*;
 
 /// Get all topics
-pub async fn get_topics(Json(_params): Json<GetTopicsRequest>) -> Result<Json<GetTopicsResponse>, StatusCode> {
+pub async fn get_topics(
+    Json(_params): Json<GetTopicsRequest>,
+) -> Result<Json<GetTopicsResponse>, StatusCode> {
     let mut conn = DB_CONNECTION.lock().unwrap();
     let db_topics = db_model::schema::topic::dsl::topic
         .select(db_model::models::Topic::as_select())
         .load(&mut *conn)
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
-    let topics = db_topics.into_iter().map(|topic| db_topic_to_topic(&topic)).collect();
+    let topics = db_topics
+        .into_iter()
+        .map(|topic| db_topic_to_topic(&topic))
+        .collect();
     let response = GetTopicsResponse {
         topics: Some(topics),
         base_response: BaseResponse::new(),
